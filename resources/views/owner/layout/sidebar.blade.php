58d47b26e367d96d5211485d85e304a4b8587dd4
Majdalshanteer <108790509+Majdalshanteer@users.noreply.github.com>
2022-10-23 21:56:31 +0300

    fix links and contact form

--- resources/views/owner/layout/sidebar.blade.php
+++ resources/views/owner/layout/sidebar.blade.php
@@ -186,7 +186,7 @@
             </li>
 
             <!-- Support -->
-            <li class="menu-header small text-uppercase"><span class="menu-header-text">Support </span></li>
+            {{-- <li class="menu-header small text-uppercase"><span class="menu-header-text">Support </span></li>
             <!-- Forms -->
             <li class="menu-item">
               <a href="/Complains" class="menu-link ">
@@ -203,6 +203,6 @@
               
             </li>
             
-          </ul>
+          </ul> --}}
         </aside>
         <!-- / Menu -->
